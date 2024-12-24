157839b0715557599c574ced892e0eac8b660cc3
Anirudh138022 <anirudhkheterpal20@gmail.com>
2024-12-24 23:50:17 +0530

    Update

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+// Root endpoint
 app.get('/', (req, res) => {
   res.send('Bot is alive!');
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
